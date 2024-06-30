c40ccd2fa7131aac5fb998c4cd167aea4954cd5f
Ayano Fujita <cathy.ayano@gmail.com>
2024-06-30 17:14:22 +0900

    wikinimous with resources

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,46 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    new_article = Article.create(article_params)
+    if new_article.save
+      redirect_to articles_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    end
+  end
+
+  def destroy
+    redirect_to articles_path if @article.destroy
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,40 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "cleaning db..."
+Article.destroy_all
+
+puts "generating articles..."
+
+10.times do
+  # content = Faker::Lorem.paragraph
+  # content += "\n #{Faker::Space.star}"
+  # content += "\n #{Faker::Lorem.paragraph_by_chars(number: rand(70..256))}"
+  # content += "\n #{Faker::Lorem.paragraph_by_chars(number: rand(70..256))}"
+  # content += "\n #{Faker::Lorem.paragraph_by_chars(number: rand(70..256))}"
+  # content += "\n #{Faker::Space.company}"
+  # content += "\n #{Faker::Lorem.paragraph(sentence_count: 3)}"
+  # content += "\n #{Faker::Music::RockBand.name}"
+  # content += "\n #{Faker::Lorem.paragraph_by_chars(number: rand(70..256))}"
+  # content += "\n #{Faker::Lorem.paragraph_by_chars(number: rand(70..256))}"
+  # content += "\n #{Faker::Lorem.paragraph_by_chars(number: rand(70..256))}"
+
+  content = "<p>#{Faker::Lorem.paragraph}</p>
+<h2>#{Faker::Space.star}</h2>
+<p>#{Faker::Lorem.paragraph_by_chars(number: rand(70..256))}</p>
+<p>#{Faker::Lorem.paragraph_by_chars(number: rand(70..256))}</p>
+<p>#{Faker::Lorem.paragraph_by_chars(number: rand(70..256))}</p>
+<h2>#{Faker::Space.company}</h2>
+<p>
+  #{Faker::Lorem.paragraph(sentence_count: 3)}
+  <a href="">#{Faker::Music::RockBand.name}</a>
+  #{Faker::Lorem.paragraph_by_chars(number: rand(70..256))}
+</p>
+<p>#{Faker::Lorem.paragraph_by_chars(number: rand(70..256))}</p>
+<p>#{Faker::Lorem.paragraph_by_chars(number: rand(70..256))}</p>"
+
+  Article.create!(title: Faker::Space.agency, content: content)
+end
+
+puts "Created #{Article.count} articles!"
